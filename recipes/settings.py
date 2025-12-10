797cc1e991c9645b6e02a9fb0e5cc43250d5eff5
Alex Fricker <alex@alexfricker.com>
2025-12-09 21:35:35 -0500

    update forwarded host

--- recipes/settings.py
+++ recipes/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost:5173"
 ).split(",")
+USE_X_FORWARDED_HOST = True
 
 
 # Application definition
